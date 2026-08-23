68433f382e315d5cd81489ce783245362e592cd0
snehamahat211 <snehamahat211@gmail.com>
2026-08-23 17:18:55 +0545

    commit the history

---
+++ locators/LoginLocators.ts
@@ -0,0 +1,9 @@
+export const LoginLocators=
+{
+    userNameInput :"#user-name",
+    passwordInput:"#password",
+    loginButton:"#login-button"
+
+
+
+}
---
+++ pages/LoginPage.ts
@@ -0,0 +1,18 @@
+import { LoginLocators } from "../locators/LoginLocators";
+import {Page} from "@playwright/test";
+
+export class LoginPage
+{
+    constructor(private page : Page)
+    {
+
+    }
+
+    async login(username :string,password :string)
+    {
+        await this.page.fill(LoginLocators.userNameInput,username);
+        await this.page.fill(LoginLocators.passwordInput,password);
+        await this.page.click(LoginLocators.loginButton)  
+    }
+}
+
