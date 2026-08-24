21d3c04a2508480b2314bae2a26397b102c37ce3
snehamahat211 <snehamahat211@gmail.com>
2026-08-24 07:47:24 +0545

    Pom file added for logout and about page

---
+++ locators/ProductPageLocators.ts
@@ -0,0 +1,6 @@
+export const productPageLocators ={
+    settingIcon : "#react-burger-menu-btn",
+    logoutlink :"logout_sidebar_link",
+    aboutlink:"about_sidebar_link"
+
+}
---
+++ pages/ProductPage.ts
@@ -0,0 +1,18 @@
+import { Page } from "@playwright/test";
+import { productPageLocators } from "../locators/ProductPageLocators";
+
+export class ProductPageLocators{
+    constructor(private page:Page){}
+    async logout()
+    {
+        await this.page.click(productPageLocators.settingIcon);
+        await this.page.click(productPageLocators.logoutlink)
+    }
+
+    async openAboutPage()
+    {
+        await this.page.click(productPageLocators.settingIcon);
+        await this.page.click(productPageLocators.settingIcon)
+    }
+    
+}
