f7d8cde94affd837187108aeff1cbd8d21a56bcd
snehamahat211 <snehamahat211@gmail.com>
2026-09-01 11:18:03 +0545

    locators for shopping cart

---
+++ locators/cartPageLocators.ts
@@ -0,0 +1,12 @@
+export const cartPagelocators=
+{
+    carttitle:".title",
+    continueshop:"#continue-shopping",
+    checkout:"#checkout",
+     productNames:".inventory_item_name ",
+    productDescription:".inventory_item_desc",
+    productPrices:".inventory_item_price",
+
+
+
+}
