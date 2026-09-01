559c9c95a5e0f2b06cef5ada4f670bf7528c9b92
snehamahat211 <snehamahat211@gmail.com>
2026-09-01 22:31:01 +0545

    add new pages

---
+++ tests/cartPage.spec.ts
@@ -0,0 +1,59 @@
+import { test, expect } from '@playwright/test'
+import { BASE_URL, USERNAME, PASSWORD } from '../utils/envConfig'
+import { ProductPage } from '../pages/ProductPage'
+import { LoginPage } from '../pages/LoginPage'
+import { productPageLocators } from '../locators/ProductPageLocators'
+import { LoginLocators } from '../locators/LoginLocators'
+import { productsToCart } from '../test-data/products'
+import { cartPagelocators } from '../locators/cartPageLocators'
+import { CartPage } from '../pages/CartPage'
+
+
+test.describe("Product Page Validation", () => {
+    let loginPage: LoginPage
+    let productPage: ProductPage
+    let cartpage:CartPage
+
+    //runs before other tests
+    test.beforeEach(async ({ page }) => {
+        loginPage = new LoginPage(page);
+        productPage = new ProductPage(page);
+        cartpage=new CartPage(page);
+
+        await page.goto(BASE_URL);
+        await loginPage.login(USERNAME, PASSWORD);
+        await expect(page).toHaveURL("https://www.saucedemo.com/inventory.html");
+    })
+    test("Validate cart page URL and UI Elements",async({page})=>
+    {
+        await productPage.addFirstProductToCart();
+        await productPage.clickOnCartLink();
+        await expect(page).toHaveURL("https://www.saucedemo.com/cart.html")
+        cartpage.getCartPageElements()
+    })
+
+    test("Validate Continue Shopping Functionality",async({page})=>
+    {
+
+    })
+
+    test("Validate Single Product in the Cart Page",async({page})=>
+    {
+
+    })
+
+    test("validate All products added to the Cart Page",async({page})=>
+    {
+
+    })
+    test("Validate specific Products added to the Cart Page",async({page})=>
+    {
+
+
+    })
+    test("Validate Remove Product Functionality",async({page})=>
+    {
+
+    })
+
+})
