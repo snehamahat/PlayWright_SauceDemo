ab70672714b3d7697e4d691e12da706eaa2e09c0
snehamahat211 <snehamahat211@gmail.com>
2026-09-04 17:36:01 +0545

    completed checkoutPage

---
+++ pages/CheckoutPage.ts
@@ -0,0 +1,40 @@
+import { Page } from "@playwright/test";
+import { CheckOutPageLocators } from "../locators/checkoutPageLocators";
+
+export class checkoutPageLocators
+{
+    constructor(private page:Page){}
+
+    async getCheckoutElements()
+    {
+        return{
+            pageInfo :this.page.locator(CheckOutPageLocators.pageInfo),
+            cancel :this.page.locator(CheckOutPageLocators.cancelButton),
+            continue:this.page.locator(CheckOutPageLocators.continueButton),}
+
+    }
+
+    async fillCheckOutDetails(firstName:string,lastName:string,postalCode:string)
+    {
+        await this.page.fill(CheckOutPageLocators.firstName,firstName);
+        await this.page.fill(CheckOutPageLocators.lastName,lastName);
+        await this.page.fill(CheckOutPageLocators.postalCode,postalCode);
+    }
+
+    async clickCancel()
+    {
+        await this.page.click(CheckOutPageLocators.cancelButton);
+    }
+
+    async clickOnContinue()
+    {
+        await this.page.click(CheckOutPageLocators.continueButton);
+    }
+
+    
+
+
+
+
+}
+
